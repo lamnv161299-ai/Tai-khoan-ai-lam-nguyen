717ad559b82c6839219cbb8ac37bc1b17321f219
Lam Nguyen <lam.nv161299@gmail.com>
2026-03-17 20:26:51 +0700

    Use Render backend URL

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-const API_BASE = "http://localhost:3000";
+// const API_BASE = "http://localhost:3000";
 // const API_BASE = "https://ten-backend-cua-ban.onrender.com";
+const API_BASE = "https://tai-khoan-ai-backend-v2.onrender.com";
